717884b243ef2ef94cebfaa0f3a661aae6dbfa9f
Valerija Grigorjeva <vgrigorjeva@gmail.com>
2018-04-22 21:04:14 +0200

    EN LOCALE FIX

--- src/i18n/locales/en.js
+++ src/i18n/locales/en.js
@@ -10,7 +10,7 @@ export default {
   myArrows: 'MY ARROWS',
   myBows: 'MY BOWS',
   calendar: 'CALENDAR',
-  myCompetitions: 'MY TRAININGS',
+  myCompetitions: 'MY COMPETITIONS',
   statistics: 'STATISTICS',
   myTrainings: 'MY TRAININGS',
   home: 'HOME',
