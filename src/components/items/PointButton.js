201fde282dabc5903cd2dc8e2fff0b36cc8712e0
Valerija Grigorjeva <vgrigorjeva@gmail.com>
2018-05-20 13:32:06 +0200

    point button background and text color depending on point value

--- src/components/items/PointButton.js
+++ src/components/items/PointButton.js
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { TouchableHighlight, Text } from 'react-native';
 import PropTypes from 'prop-types';
 
 import { colors } from '../../styles/general';
 import styles from '../../styles/items/buttons';
 
-const PointButton = ({ onPress, number }) => (
-  <TouchableHighlight
-    onPress={onPress}
-    underlayColor={colors.underlayColor}
-    style={styles.pointButton}
-  >
-    <Text>{number}</Text>
-  </TouchableHighlight>
-);
+export default class PointButton extends Component {
+  render() {
+    const { onPress, number } = this.props;
+    var color = 'black';
+    var textColor = 'black';
+    if (number === 1 || number === 2) {
+      color = 'white';
+    } else if (number === 3 || number === 4) {
+      color = 'black';
+      textColor = 'white';
+    } else if (number === 5 || number === 6) {
+      color = 'blue';
+    } else if (number === 7 || number === 8) {
+      color = 'red';
+    } else if (number === 9 || number === 10) {
+      color = 'yellow';
+    }
+
+    return (
+      <TouchableHighlight
+        onPress={onPress}
+        underlayColor={colors.underlayColor}
+        style={[styles.pointButton, { backgroundColor: color }]}
+      >
+        <Text style={{ color: textColor }}>{number}</Text>
+      </TouchableHighlight>
+    );
+  }
+}
 
 PointButton.propTypes = {
   onPress: PropTypes.func.isRequired,
   number: PropTypes.number.isRequired,
 };
-
-export default PointButton;
